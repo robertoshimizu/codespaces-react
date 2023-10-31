7b02627836f5c2c101dbd1de96160b2143c84d6f
Roberto Shimizu <32114050+robertoshimizu@users.noreply.github.com>
2023-10-31 11:13:31 +0000

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+const Header = ()=>{
+    return (
+        <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    )
+}
+
+export default Header;
